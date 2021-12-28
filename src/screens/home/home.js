23ec8b109fee08ce9f0ed02d8ea265666d72d1b9
fatiherguder <fatih.erguder@gmail.com>
2021-12-28 15:09:18 +0300

    bug-fix

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,6 +1,6 @@
 
 import React,{useEffect, useState} from 'react';
-import {Dimensions, SafeAreaView, Image, StyleSheet, ScrollView,View,} from 'react-native';
+import {Dimensions, SafeAreaView, Image, StyleSheet, ScrollView,View,Text} from 'react-native';
 import CustomButton from '../../components/button';
 import CustomCard from '../../components/card';
 import HomeNavbar from '../../components/homeNavbar';
@@ -8,28 +8,33 @@ import auth from '@react-native-firebase/auth';
 import { getFirestore, collection, getDoc, doc } from "firebase/firestore"
 
 
-const Home = (props) => {
-
+const Home = (props, route) => {
   const db = getFirestore();
   const userId = auth().currentUser.uid;
 
   const [userData, setUserData] = useState();
-  const [marketData, setMarketData] = useState();
+  const [marketData, setMarketData] = useState([]);
+  const [loading, setLoading] = useState(true);
   
   const getUserData = async () => {
-    const docSnap = await getDoc(doc(db, "users", userId))
+    const docSnap = await getDoc(doc(db, "users", userId));
     setUserData(docSnap.data());
     console.log(userData)
+    setLoading(false)
   }
 
-  const getMarketData = async () => {
-    const marketSnap = await getDoc(doc(db, "markets", docSnap.data().markets))
-    setMarketData(marketSnap.data())
-    console.log("markets : " + marketData)
+  const getMarketData = () => {
+    const array = [];
+    userData.markets.map(async (item) => {
+      const marketSnap = await getDoc(doc(db, "markets", item));
+      await array.push(marketSnap.data())
+      setMarketData(array);
+      return (console.log(marketData));
+    })
   }
 
   useEffect( () => {
-     getUserData().then(getMarketData())
+     getUserData().then(() => getMarketData())
   })
 
   var food1 = require('../../assets/img/food-1.jpg')
@@ -44,14 +49,15 @@ const Home = (props) => {
       <SafeAreaView>
         <HomeNavbar navigation={props.navigation} name={userData?.name}/>
           <View  style={styles.scrollArea}>
-            <ScrollView style={styles.scroll}>
-              <CustomCard img={food1} title={'SteakHouse'} score={'4.5'}  navigation={props.navigation}/>
-              <CustomCard img={food2} title={'Ambrosia Cafe'} score={'4.2'}  navigation={props.navigation}/>
-              <CustomCard img={food3} title={'Kapsül BurgerHouse'} score={'4.7'}  navigation={props.navigation}/>
-              <CustomCard img={food4} title={'Koniçiva'} score={'4.1'}  navigation={props.navigation}/>
-              <CustomCard img={food5} title={'Yusuf Kahvaltı Evi'} score={'2.8'}  navigation={props.navigation}/>
-              <CustomCard img={food6} title={'Kopernik Pizza'} score={'4.3'}  navigation={props.navigation}/>
+            {
+              loading ? <Text>Loading ...</Text> : <ScrollView style={styles.scroll}>
+                {
+                  marketData.map(market => {
+                    return <CustomCard img={market.img} title={market.name} score={market.score}  navigation={props.navigation}/>
+                  })
+                }
             </ScrollView>
+            }
           </View>
       </SafeAreaView>
     </View>
